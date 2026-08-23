606374218928516601120a9ffd9cd25a57a9aa80
julinejdvglory-droid <juline.jdv.glory@gmail.com>
2026-08-23 12:26:26 +0100

    chore: Initialize 09-frontend App component

---
+++ packages/09-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>JDV GLOBAL CENTER</h1>
+      <p>Votre monde, connecté</p>
+    </div>
+  );
+};
+
+export default App;
