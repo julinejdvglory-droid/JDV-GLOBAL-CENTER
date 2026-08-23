2a6b8d645d0d180ead1cefbec46fe1edc30598e2
julinejdvglory-droid <juline.jdv.glory@gmail.com>
2026-08-23 12:25:59 +0100

    chore: Initialize 10-mobile App component

---
+++ packages/10-mobile/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const App: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>JDV GLOBAL CENTER</Text>
+      <Text>Votre monde, connecté</Text>
+    </View>
+  );
+};
+
+export default App;
